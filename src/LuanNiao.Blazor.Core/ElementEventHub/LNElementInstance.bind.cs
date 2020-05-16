3394c546f9dcd7bcb40048a3fed216e5d14f726a
BenLampson(赵元源) <58603432@qq.com>
2020-05-16 22:09:06 +0800

    change the code style
    change the code style

--- src/LuanNiao.Blazor.Core/ElementEventHub/LNElementInstance.bind.cs
+++ src/LuanNiao.Blazor.Core/ElementEventHub/LNElementInstance.bind.cs
@@ -28,193 +28,86 @@ namespace LuanNiao.Blazor.Core.ElementEventHub
 
                 }
             }
+
+            Dictionary<int, LNElementEvent> eventPool = null;
             foreach (var methodName in methodNames)
             {
                 var typeInfo = instance.GetType();
                 var method = typeInfo.GetMethod(methodName);
                 var attr = method.GetCustomAttribute<LNElementEventAttribute>();
-                if (attr is OnClickEventAttribute clickEventAttribute)
+                switch (attr)
                 {
-                    lock (_clickEventPool)
-                    {
-                        if (!_clickEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _clickEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
+                    case OnClickEventAttribute _ :
+                        eventPool = _clickEventPool;
+                        break;
+                    case OnMouseOverEventAttribute  _:
+                        eventPool = _onMouseOverEventPool;
+                        break;
+                    case OnMouseEnterEventAttribute  _:
+                        eventPool = _onMouseEnterEventPool;
+                        break;
+                    case OnMouseDownEventAttribute _:
+                        eventPool = _onMouseDownEventPool;
+                        break;
+                    case OnMouseUpEventAttribute _:
+                        eventPool = _onMouseUpEventPool;
+                        break;
+                    case OnMouseMoveEventAttribute _:
+                        eventPool = _onMouseMoveEventPool;
+                        break;
+                    case OnMouseOutEventAttribute _:
+                        eventPool = _onMouseOutEventPool;
+                        break;
+                    case OnContextMenuEventAttribute _:
+                        eventPool = _onContextMenuEventPool;
+                        break;
+                    case OnBlurEventAttribute _:
+                        eventPool = _onBlurEventPool;
+                        break;
+                    case OnChangeEventAttribute _:
+                        eventPool = _onChangeEventPool;
+                        break;
+                    case OnFocusEventAttribute _:
+                        eventPool = _onFocusEventPool;
+                        break;
+                    case OnFocusInEventAttribute _:
+                        eventPool = _onFocusInEventPool;
+                        break;
+                    case OnFocusOutEventAttribute _:
+                        eventPool = _onFocusOutEventPool;
+                        break;
+                    case OnInputEventAttribute _:
+                        eventPool = _onInputEventPool;
+                        break;
+                    case OnKeyDownEventAttribute _:
+                        eventPool = _onKeyDownEventPool;
+                        break;
+                    case OnKeypressEventAttribute _:
+                        eventPool = _onKeypressEventPool;
+                        break;
+                    case OnKeyupEventAttribute _:
+                        eventPool = _onKeyupEventPool;
+                        break;
+                    case OnScrollEventAttribute _:
+                        eventPool = _onScrollEventPool;
+                        break;
+                    default:
+                        break;
                 }
-                else if (attr is OnMouseOverEventAttribute onMouseOver)
+
+
+                lock (eventPool)
                 {
-                    lock (_onMouseOverEventPool)
+                    if (!eventPool.ContainsKey(instance.CreateSequence))
                     {
-                        if (!_onMouseOverEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onMouseOverEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnMouseEnterEventAttribute  onMouseEnter)
-                {
-                    lock (_onMouseEnterEventPool)
-                    {
-                        if (!_onMouseEnterEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onMouseEnterEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnMouseDownEventAttribute  onMouseDown)
-                {
-                    lock (_onMouseDownEventPool)
-                    {
-                        if (!_onMouseDownEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onMouseDownEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnMouseUpEventAttribute onMouseUp)
-                {
-                    lock (_onMouseUpEventPool)
-                    {
-                        if (!_onMouseUpEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onMouseUpEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnMouseMoveEventAttribute  onMouseMove)
-                {
-                    lock (_onMouseMoveEventPool)
-                    {
-                        if (!_onMouseMoveEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onMouseMoveEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnMouseOutEventAttribute  onMouseOut)
-                {
-                    lock (_onMouseOutEventPool)
-                    {
-                        if (!_onMouseOutEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onMouseOutEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnContextMenuEventAttribute  onContextMenu)
-                {
-                    lock (_onContextMenuEventPool)
-                    {
-                        if (!_onContextMenuEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onContextMenuEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnBlurEventAttribute  onBlurEvent)
-                {
-                    lock (_onBlurEventPool)
-                    {
-                        if (!_onBlurEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onBlurEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnChangeEventAttribute  onChangeEvent)
-                {
-                    lock (_onChangeEventPool)
-                    {
-                        if (!_onChangeEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onChangeEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnFocusEventAttribute  onFocusEvent)
-                {
-                    lock (_onFocusEventPool)
-                    {
-                        if (!_onFocusEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onFocusEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnFocusInEventAttribute  onFocusInEvent)
-                {
-                    lock (_onFocusInEventPool)
-                    {
-                        if (!_onFocusInEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onFocusInEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnFocusOutEventAttribute  onFocusOutEvent)
-                {
-                    lock (_onFocusOutEventPool)
-                    {
-                        if (!_onFocusOutEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onFocusOutEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnInputEventAttribute onInputEvent)
-                {
-                    lock (_onInputEventPool)
-                    {
-                        if (!_onInputEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onInputEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnKeyDownEventAttribute  onKeyDownEvent)
-                {
-                    lock (_onKeyDownEventPool)
-                    {
-                        if (!_onKeyDownEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onKeyDownEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnKeypressEventAttribute onKeypressEvent)
-                {
-                    lock (_onKeypressEventPool)
-                    {
-                        if (!_onKeypressEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onKeypressEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnKeyupEventAttribute  onKeyupEvent)
-                {
-                    lock (_onKeyupEventPool)
-                    {
-                        if (!_onKeyupEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onKeyupEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
-                    }
-                }
-                else if (attr is OnScrollEventAttribute  onScrollEvent)
-                {
-                    lock (_onScrollEventPool)
-                    {
-                        if (!_onScrollEventPool.ContainsKey(instance.CreateSequence))
-                        {
-                            _onScrollEventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
-                        }
+                        eventPool.Add(instance.CreateSequence, new LNElementEvent(method, attr));
                     }
                 }
 
+
             }
+
+            
             return this;
         }
     }
